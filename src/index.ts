b101ebb612fe174d78f08615360b9bdd6cd90e59
Kholodkov, Anton <anton.kholodkov@aliexpress.ru>
2021-07-19 19:40:25 +0300

    api v1 prefix

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import fpr from 'fastify-print-routes'
 import { Update } from 'telegraf/typings/core/types/typegram'
 import bot from './bot';
 import getDatabase from './db'
-// import sqlite3 from 'sqlite3'
 
 dotenv.config()
+
+const API_V1_PREFIX: string = '/api/v1'
 
 const { TOKEN, SERVER, DB_PATH } = process.env;
 
@@ -23,53 +24,47 @@ if (!DB_PATH) {
   throw Error('No DB_PATH!')
 }
 
-const API_V1_PREFIX: string = '/api/v1'
 
 const start = async () => {
 
-  const server: FastifyInstance = Fastify({ logger: pino() })
-
-  server.log.info({ msg: `Start with bot token: ${TOKEN}` })
-
-  server.register(fpr)
-
   try {
+
+    const server: FastifyInstance = Fastify({ logger: pino() })
+
+    server.log.info({ msg: `Start with bot token: ${TOKEN}` })
+
+    server.register(fpr)
+
     const db = await getDatabase(DB_PATH || 'db')
     server.decorate('db', db);
     server.decorate('bot', bot(TOKEN, db))
-  } catch (e) {
-    console.error(e);
-    process.exit(200);
-  }
 
-  const SECRET_PATH: string = `${API_V1_PREFIX}/telegraf/${server.bot.secretPathComponent()}`
+    const SECRET_PATH: string = `${API_V1_PREFIX}/telegraf/${server.bot.secretPathComponent()}`
 
-  server.post(SECRET_PATH, (req: FastifyRequest, res: FastifyReply) => {
-    server.bot.handleUpdate(req.body as Update, res.raw);
-  })
-
-  type MsgRequest = FastifyRequest<{
-    Body: { msg: string }
-  }>
-
-  server.post('${API_V1_PREFIX}/msg', async (req: MsgRequest, reply) => {
-
-    interface Row {
-      chat_id: string
-    }
-
-    server.db.each('select chat_id from chats', (err, row: Row) => {
-      if (err) {
-        throw err;
-      }
-
-      server.bot.telegram.sendMessage(row.chat_id, req.body.msg)
+    server.post(SECRET_PATH, (req: FastifyRequest, res: FastifyReply) => {
+      server.bot.handleUpdate(req.body as Update, res.raw);
     })
 
-    return { status: 'Success!' }
-  })
+    type MsgRequest = FastifyRequest<{
+      Body: { msg: string }
+    }>
 
-  try {
+    server.post(`${API_V1_PREFIX}/msg`, async (req: MsgRequest, reply) => {
+      interface Row {
+        chat_id: string
+      }
+
+      server.db.each('select chat_id from chats', (err, row: Row) => {
+        if (err) {
+          throw err;
+        }
+
+        server.bot.telegram.sendMessage(row.chat_id, req.body.msg)
+      })
+
+      return { status: 'Success!' }
+    })
+
     await server.bot.telegram.setWebhook(`${SERVER}${SECRET_PATH}`)
     await server.listen(<string>process.env.PORT || 8088, <string>process.env.HOST || '127.0.0.1')
   } catch (err) {
