2589ad2309e6498304bb2d24113fbc8c20bfbf62
Kholodkov, Anton <anton.kholodkov@aliexpress.ru>
2021-08-06 18:02:27 +0300

    markdown

--- src/controllers/msg/hook.ts
+++ src/controllers/msg/hook.ts
@@ -6,51 +6,90 @@ import { Chat } from '../../entity/Chat'
 
 export default function (fastify: FastifyInstance) {
 
-    const MsgParams = Type.Object({
-        msg: Type.String(),
-    })
+  const MsgParams = Type.Object({
+    msg: Type.String(),
+    type: Type.Optional(Type.KeyOf(
+      Type.Object({
+        text: Type.String(),
+        html: Type.String(),
+        markdown: Type.String()
+      }))
+    )
+  })
 
-    type MsgParamsType = Static<typeof MsgParams>
+  type MsgParamsType = Static<typeof MsgParams>
 
-    const KeyParams = Type.Object({
-        key: Type.String(),
-    })
+  const KeyParams = Type.Object({
+    key: Type.String(),
 
-    type KeyParamsType = Static<typeof KeyParams>
+  })
 
-    const schema = {
-        params: KeyParams,
-        body: MsgParams,
-        response: {
-            200: {
-                statusCode: { type: 'number' }
-            }
-        }
+  type KeyParamsType = Static<typeof KeyParams>
+
+  const schema = {
+    params: KeyParams,
+    body: MsgParams,
+    response: {
+      200: {
+        statusCode: { type: 'number' }
+      }
     }
+  }
 
-    type MsgRequest = FastifyRequest<{
-        Params: KeyParamsType,
-        Body: MsgParamsType
-    }>
+  type MsgRequest = FastifyRequest<{
+    Params: KeyParamsType,
+    Body: MsgParamsType
+  }>
 
-    fastify.post(`${API_V1_PREFIX}/msg/:key`, { schema }, async (req: MsgRequest, res: FastifyReply) => {
+  fastify.post(`${API_V1_PREFIX}/msg/:key`, { schema }, async (req: MsgRequest, res: FastifyReply) => {
 
-        const holder = fastify.bots.find((holder) => (holder.slug === req.params.key))
 
-        if (holder) {
+    const holder = fastify.bots.find((holder) => (holder.slug === req.params.key))
 
-            const chats = await Chat.find({ bot_id: holder.id })
+    if (holder) {
 
-            await Promise.all([chats.map((chat: Chat) => {
-                return holder.bot.telegram.sendMessage(chat.chat_id, req.body.msg, { parse_mode: "MarkdownV2" })
-            })])
+      const chats = await Chat.find({ bot_id: holder.id })
 
-            return {
-                statusCode: 200
-            }
+      chats.forEach(async (chat: Chat) => {
+        try {
+          switch (req.body.type) {
+            case 'markdown':
+              await holder.bot.telegram.sendMessage(chat.chat_id, req.body.msg, { parse_mode: "MarkdownV2" })
+              break
 
-        } else {
-            throw createError(404, 'Bot not found!')
+            case 'html':
+              await holder.bot.telegram.sendMessage(chat.chat_id, req.body.msg, { parse_mode: "HTML" })
+              break
+
+            default:
+            case 'text':
+              await holder.bot.telegram.sendMessage(chat.chat_id, req.body.msg)
+              break
+
+          }
+
+        } catch (e) {
+          if ((e.response.ok == false) && (e.response.error_code == 403)) {
+
+            const chatRec = new Chat();
+            chatRec.bot_id = holder.id
+            chatRec.chat_id = chat.chat_id
+
+            await chatRec.remove()
+
+          }
         }
-    })
+
+
+      })
+
+
+      return {
+        statusCode: 200
+      }
+
+    } else {
+      throw createError(404, 'Bot not found!')
+    }
+  })
 }
